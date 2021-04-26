66cdbce1520efa69000467337bf471b52bff4d60
mcakyerima <mcakyerima@gmail.com>
2021-04-26 23:51:03 +0100

    Login Page

---
+++ src/Components/Login.js
@@ -0,0 +1,13 @@
+import styled from 'styled-Components';
+
+import React from 'react'
+
+const Login = (props) => {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
+
+export default Login
